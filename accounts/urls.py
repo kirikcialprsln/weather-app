a7c1821b6f6daa425425825ebd3525365c203e89
Alparslan KIRIKCI <kirikci.alprsln@gmail.com>
2022-07-12 22:47:31 +0200

    logout view created

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,9 @@
+from django.contrib.auth.decorators import login_required
 from django.urls import path
 from accounts import views
 
 
 urlpatterns = [
     path('login/', views.LoginPage.as_view(), name='login'),
+    path('logout/', login_required(views.logout_view), name='logout'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth import logout
 
 
 def home(request):
     return render(request, 'pages/home.html')
+
+
+def logout_view(request):
+    logout(request)
