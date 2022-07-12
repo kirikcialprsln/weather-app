0275eb2129d3a97ddd0a73ba490e2cf86c7f3acb
Alparslan KIRIKCI <kirikci.alprsln@gmail.com>
2022-07-12 22:47:21 +0200

    templates updated

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,12 @@
 from django.contrib.auth.views import LoginView
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout
 
 
 class LoginPage(LoginView):
     template_name = 'accounts/login.html'
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
